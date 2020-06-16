e1ec1a0a877252abc5394e13b55b5bcf605c08fb
drinkingstraw33 <60614681+drinkingstraw33@users.noreply.github.com>
2020-06-16 20:21:18 +0900

    database 수정
    fixes 데이터베이스에 휴대폰 이름 삽입

--- mainpage/db/schema.rb
+++ mainpage/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200610061201) do
+ActiveRecord::Schema.define(version: 20200616105229) do
 
   create_table "fixes", force: :cascade do |t|
     t.string   "phone"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20200610061201) do
     t.integer  "shop_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "phonename"
     t.index ["shop_id"], name: "index_fixes_on_shop_id"
   end
 
